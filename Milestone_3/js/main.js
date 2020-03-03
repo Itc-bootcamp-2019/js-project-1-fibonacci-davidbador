a4bfb0d32b7e308267819c6e667c877f57c5fc97
David Bador <david.bador@gmail.com>
2020-03-03 16:00:25 +0200

    changes to the JS file

--- Milestone_3/js/main.js
+++ Milestone_3/js/main.js
@@ -3,15 +3,19 @@ function fibonacciSequence (x) {
     let secondNum = 1;
     let y = 1;
     
-    for (let i = 0; i < x; i++) {
-        y = firstNum + secondNum
-        secondNum = firstNum;
-        firstNum = y;
+    if (x >= 1) {
+        for (let i = 0; i < x; i++) {
+            y = firstNum + secondNum
+            secondNum = firstNum;
+            firstNum = y;
+        }
+    } else if (x == 0) {
+        return 0;
     }
     return y
 }
 
 let htmlX = document.getElementById('firstNumber');
 let htmlY = document.getElementById('secondNumber');
-htmlX.innerText = 13;
+htmlX.innerText = 3;
 htmlY.innerText = fibonacciSequence(htmlX.innerText);
