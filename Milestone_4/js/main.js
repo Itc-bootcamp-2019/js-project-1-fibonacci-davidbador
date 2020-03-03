ae9d22d55e8278b73267d210f63b48cd281ce184
David Bador <david.bador@gmail.com>
2020-03-03 10:57:17 +0200

    Updates to JS for better functionality

--- Milestone_4/js/main.js
+++ Milestone_4/js/main.js
@@ -11,13 +11,18 @@ function fibonacciSequence (x) {
     return y
 }
 
+let re = /^\d+$/;
 const inputField = document.getElementById('chosenNumber');
 const button = document.getElementById('buttonForResult');
 const answer = document.getElementById('resultArea');
 button.addEventListener('click', fibonacciResult)
 
 function fibonacciResult () {
-    if (inputField.value !== NaN) {
+    if (re.test(inputField.value) === true && inputField.value >= 1) {
         answer.innerText = fibonacciSequence(inputField.value);
+    } else if (re.test(inputField.value) === true && inputField.value == 0)  {
+        answer.innerText = 0;
+    } else if (re.test(inputField.value) === false || inputField.value < 0) {
+        answer.innerText = 'Please enter a valid number!';
     }
 }
