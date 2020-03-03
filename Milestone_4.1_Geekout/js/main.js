bbe3453bb58f136084bf10bd3ee20e6b75814f43
David Bador <david.bador@gmail.com>
2020-03-03 14:35:54 +0200

    My updated code for Milestone 4.1 Geekout

---
+++ Milestone_4.1_Geekout/js/main.js
@@ -0,0 +1,23 @@
+function fibonacciSequence(x) {
+    if (x > 1) {
+        return fibonacciSequence(x - 1) + fibonacciSequence(x - 2);
+    } else if (x == 1) {
+        return 1;
+    } else if (x == 0) {
+        return 0;
+    }
+}
+
+let re = /^\d+$/;
+const inputField = document.getElementById('chosenNumber');
+const button = document.getElementById('buttonForResult');
+const answer = document.getElementById('resultArea');
+button.addEventListener('click', fibonacciResult)
+
+function fibonacciResult() {
+    if (re.test(inputField.value) === true && inputField.value >= 0) {
+        answer.innerText = fibonacciSequence(inputField.value);
+    } else if (re.test(inputField.value) === false || inputField.value < 0) {
+        answer.innerText = 'Please enter a valid number that is equal to or greater than 0!';
+    }
+}
