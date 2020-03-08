88467643d38678bf9ab16de98108c9729c2d22be
David Bador <david.bador@gmail.com>
2020-03-08 10:53:37 +0200

    changes to error handling in JS

--- Milestone_5/js/main.js
+++ Milestone_5/js/main.js
@@ -1,18 +1,28 @@
+const inputField = document.getElementById('chosenNumber');
+const button = document.getElementById('buttonForResult');
+const answer = document.getElementById('resultArea');
+let re = /^\d+$/;
+
 function fibonacciSequence(x) {
     fetch('http://localhost:5050/fibonacci/' + x).then(response => {
         return response.json()
     }).then(data => {
         answer.innerText = data.result;
-    }).catch(err => {
-        answer.innerText = 'Please enter a valid number!'
+    }).catch(err => err.text()).then((errorMessage) => {
+        if (re.test(x) === false) {
+            setTimeout(() => {
+                answer.innerText = 'Please enter a valid number';
+            }, 1000);
+        } else if (x < 1) {
+            setTimeout(() => {
+                answer.innerText = `${errorMessage}`;
+            }, 1000);
+        }
     })
 }
-
-const inputField = document.getElementById('chosenNumber');
-const button = document.getElementById('buttonForResult');
-const answer = document.getElementById('resultArea');
-button.addEventListener('click', fibonacciResult)
 
 function fibonacciResult() {
     fibonacciSequence(inputField.value);
 }
+
+button.addEventListener('click', fibonacciResult)
