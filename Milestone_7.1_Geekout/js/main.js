01158df8a17e1cfbf893711af04510c1a984ad30
David Bador <david.bador@gmail.com>
2020-03-10 17:37:01 +0200

    made changes to JS code and compiled my window load code under one function with an event listener

--- Milestone_7.1_Geekout/js/main.js
+++ Milestone_7.1_Geekout/js/main.js
@@ -4,13 +4,13 @@ const loaderTimeline = document.getElementById('loaderTimeline');
 const inputField = document.getElementById('chosenNumber');
 const button = document.getElementById('buttonForResult');
 const answer = document.getElementById('resultArea');
-const alert = document.getElementById('alert');
+const alertOver50 = document.getElementById('alert');
 const history = document.getElementById('resultsHistory');
 const re = /^\d+$/;
 
 // JS Class Modifiers
 loader.classList.add('hide');
-alert.classList.add('hide');
+alertOver50.classList.add('hide');
 loaderTimeline.classList.add('show');
 
 // Asynchronous Fibonacci Calculator Function
@@ -18,8 +18,8 @@ async function fibonacciSequence(x) {
     showLoader()
     if (x > 50) {
         showLoader()
-        alert.innerText = "number can't be bigger than 50"
-    }  else if (re.test(x) === false) {
+        alertOver50.innerText = "number can't be bigger than 50"
+    } else if (re.test(x) === false) {
         setTimeout(() => {
             answer.innerText = 'Please enter a valid number';
         }, 1000);
@@ -88,7 +88,7 @@ function refreshHistory() {
 function validateInput() {
     if (inputField.value > 50) {
         setTimeout(() => {
-            alert.classList.replace('hide', 'show');
+            alertOver50.classList.replace('hide', 'show');
             answer.className = 'hide';
             inputField.classList.add('invalid');
         }, 600)
@@ -99,7 +99,7 @@ function validateInput() {
 function validNumber() {
     if (inputField.value == "") {
         inputField.classList.remove("invalid");
-        alert.classList.replace('show', 'hide');
+        alertOver50.classList.replace('show', 'hide');
         answer.classList.replace('show', 'hide');
         answer.classList.replace('error', 'hide');
     }
@@ -110,7 +110,7 @@ function showLoader() {
     validateInput()
     if (inputField.value < 50) {
         inputField.classList.remove('invalid');
-        alert.classList.replace('show', 'hide');
+        alertOver50.classList.replace('show', 'hide');
         loader.classList.replace('hide', 'show');
         answer.className = 'hide';
         setTimeout(() => {
@@ -125,16 +125,18 @@ function fibonacciResult() {
     fibonacciSequence(inputField.value)
 }
 
+// Window Loaded Completely Function
+function loadWindow() {
+    setTimeout(() => {
+        loaderTimeline.classList.replace('show', 'hide');
+    }, 2000)
+    setTimeout(() => {
+        fibonacciHistory()
+    }, 1300)
+}
+
 // Event Listeners
+window.addEventListener('load', loadWindow);
 button.addEventListener('click', fibonacciResult);
 button.addEventListener('click', refreshHistory);
 inputField.addEventListener('keyup', validNumber);
-
-// Window Loaded Completely Functions
-setTimeout(() => {
-    loaderTimeline.classList.replace('show', 'hide');
-}, 2000)
-
-window.onload = setTimeout(() => {
-    fibonacciHistory()
-}, 1300)
