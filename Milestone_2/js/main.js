ab290047e42b86ac65cc88f98926de3778648d12
David Bador <david.bador@gmail.com>
2020-04-02 18:30:09 +0300

    removed unnecessary files

--- Milestone_2/js/main.js
+++
@@ -1,7 +0,0 @@
-// getElementById variables
-const htmlX = document.getElementById('firstNumber');
-const htmlY = document.getElementById('secondNumber');
-
-// innerText identifiers
-htmlX.innerText = 'X';
-htmlY.innerText = 'Y';
