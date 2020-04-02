954df8b4458bbde38cb3dc1adc497cc23c4025f2
David Bador <david.bador@gmail.com>
2020-04-02 17:56:34 +0300

    Completed files for Milestone 4

---
+++ js/main.js
@@ -0,0 +1,37 @@
+// Global Variables
+const inputField = document.getElementById('chosenNumber');
+const button = document.getElementById('buttonForResult');
+const answer = document.getElementById('resultArea');
+const re = /^\d+$/;
+
+// Fibonacci Calculation Function
+function fibonacciSequence(x) {
+    let firstNum = 0;
+    let secondNum = 1;
+    let y = 1;
+
+    if (x >= 1) {
+        for (let i = 1; i < x; i++) {
+            y = firstNum + secondNum;
+            firstNum = secondNum;
+            secondNum = y;
+        }
+    } else if (x == 0) {
+        return 0;
+    }
+    return y;
+}
+
+// Fibonacci Calculation Result Function
+function fibonacciResult() {
+    if (re.test(inputField.value) === true && inputField.value >= 1) {
+        answer.innerText = fibonacciSequence(inputField.value);
+    } else if (re.test(inputField.value) === true && inputField.value == 0) {
+        answer.innerText = 0;
+    } else if (re.test(inputField.value) === false || inputField.value < 0) {
+        answer.innerText = 'Please enter a valid number that is equal to or greater than 0!';
+    }
+}
+
+// Event Listeners
+button.addEventListener('click', fibonacciResult);
