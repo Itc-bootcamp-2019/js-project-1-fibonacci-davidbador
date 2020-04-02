68e6fe866de0ac9ec7c35a8b385f10e0b605c201
David Bador <david.bador@gmail.com>
2020-04-02 17:32:07 +0300

    Files for Completed Milestone 2

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// getElementById variables
+const htmlX = document.getElementById('firstNumber');
+const htmlY = document.getElementById('secondNumber');
+
+// innerText identifiers
+htmlX.innerText = 'X';
+htmlY.innerText = 'Y';
