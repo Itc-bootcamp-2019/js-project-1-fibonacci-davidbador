e31ac601de4859dc1744a3afe3114b6c74153d38
David Bador <david.bador@gmail.com>
2020-04-04 17:35:06 +0300

    Files for Completed JS Project 1.1 Advanced Milestone 3

--- server/app.js
+++ server/app.js
@@ -1,20 +1,34 @@
 const express = require('express');
 const app = express();
-let array = [];
 const cors = require('cors');
 
 app.use(cors());
+
+let MongoClient = require('mongodb').MongoClient;
+let url = "mongodb://localhost:27017/";
+
+let dbo;
+
+MongoClient.connect(url, (err, db) => {
+    if (err) throw err;
+    dbo = db.db("mydb");
+})
 
 app.get('/fibonacci/:number', function (req, res) {
     let number = +req.params.number;
     let result = fibonacciRecursion(number);
     let object = { number, result, createdDate: Date.now() };
-    array.push(object);
-    res.send(array);
+    dbo.collection("fibonacci").insertOne(object, (err) => {
+        if (err) throw err;
+        res.send(object);
+    })
 })
 
 app.get('/getFibonacciResults', function (req, res) {
-    res.send(array);
+    dbo.collection("fibonacci").find({}).toArray((err, documents) => {
+        if (err) throw err;
+        res.send(documents);
+    })
 })
 
 fibonacciRecursion = (number) => {
